c98bf80b5e3193a759275d02e7bf4b311600c805
haseeb <pkhaseeb04@gmail.com>
2023-10-17 11:10:44 -0700

    yard To SqFeet

---
+++ script.mjs
@@ -0,0 +1,12 @@
+let yard = document.getElementById("yard");
+let feet = document.getElementById("feet");
+
+function yardToFt(){
+    let output =(parseFloat(yard.value)*9);
+    feet.value = parseFloat(output.toFixed(3));
+}
+
+function ftToYard(){
+    let output = (parseFloat(feet.value)*0.111111);
+    yard.value = parseFloat(output.toFixed(3));
+}
